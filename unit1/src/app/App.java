4c5fe76af6229eb629ac535b64ca995375b50b05
maysonwiome <819689@mystma.org>
2020-04-08 16:38:42 +0000

    5 movies added

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. The Godfather");
+        System.out.println("2. Friday the 13th part 4");
+        System.out.println("3. Logan");
+        System.out.println("4. Deadpool 2");
+        System.out.println("5. Halloween");
     }
 }

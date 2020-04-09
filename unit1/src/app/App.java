99bab24fbdc0761b1735ef58fb06e7c26f5e01f9
maysonwiome <819689@mystma.org>
2020-04-09 15:24:03 +0000

    data things added

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,10 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. The Godfather");
-        System.out.println("2. Friday the 13th part 4");
-        System.out.println("3. Logan");
-        System.out.println("4. Deadpool 2");
-        System.out.println("5. Halloween");
+        boolean livesWithBrothers = false;
+        byte numOfSisters = 13;
+        char firstLetterOfName = 'M';
+        short mstExpensiveFnko = 2243;
+        int numOfFnko = 134;
+        long numOfSweatshirts = 12L;
+        float numOfFish = 3.15f;
+        double numbOfScks = 10e1d;
+
+        System.out.println("number of socks I have.");
+        System.out.println(numbOfScks);
+        System.out.println("Number of fish I have");
+        System.out.println(numOfFish);
+        System.out.println("Number of Sweatshirts I own.");
+        System.out.println(numOfSweatshirts);
+        System.out.println("Number of funko pops.");
+        System.out.println(numOfFnko);
+        System.out.println("Whats my most expensive funko pop?");
+        System.out.println(mstExpensiveFnko);
+        System.out.println("First letter of my name.");
+        System.out.println(firstLetterOfName);
+        System.out.println("How many sisters do I have.");
+        System.out.println(numOfSisters);
+        System.out.println("Do I live with brothers?");
+        System.out.println(livesWithBrothers);
     }
 }

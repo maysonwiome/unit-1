2baab8f003ea3170dc0d0a2c56e85da80d85984a
maysonwiome <819689@mystma.org>
2020-04-13 15:59:06 +0000

    mad lib assinment

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,30 +2,24 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean livesWithBrothers = false;
-        byte numOfSisters = 13;
-        char firstLetterOfName = 'M';
-        short mstExpensiveFnko = 2243;
-        int numOfFnko = 134;
-        long numOfSweatshirts = 12L;
-        float numOfFish = 3.15f;
-        double numbOfScks = 10e1d;
+        
+        String person1 = "Mayson Wiome";
+        String person2 = "Zander Hasher";
+        String verb1 = "sleep";
+        String verb2 = "play";
+        String verb3 = "run";
+        String food = "toast";
+        String dayOfWeek = "friday";
+        String place1 = "home";
+        String place2 = "mcdonalds";
+        String adjective1 = "burnt";
+        String adjective2 = "great";
 
-        System.out.println("number of socks I have.");
-        System.out.println(numbOfScks);
-        System.out.println("Number of fish I have");
-        System.out.println(numOfFish);
-        System.out.println("Number of Sweatshirts I own.");
-        System.out.println(numOfSweatshirts);
-        System.out.println("Number of funko pops.");
-        System.out.println(numOfFnko);
-        System.out.println("Whats my most expensive funko pop?");
-        System.out.println(mstExpensiveFnko);
-        System.out.println("First letter of my name.");
-        System.out.println(firstLetterOfName);
-        System.out.println("How many sisters do I have.");
-        System.out.println(numOfSisters);
-        System.out.println("Do I live with brothers?");
-        System.out.println(livesWithBrothers);
+        System.out.println(person1 + " " +"is a programmer who likes to" + " " + verb1);
+        System.out.println("every" + " " + dayOfWeek + " " + " they make a" + " " + adjective1);
+        System.out.println( food + " " + "for breakfast and" + " " + verb2 + " " + "at" + " " + place1);
+        System.out.println("with" + " " + person2 + " " + "Sometimes, they go to the" + " " + adjective2);
+        System.out.println(place2 + " " + "with" + " " + person2 + " " + "so that they can" + " " + verb3);
+
     }
 }

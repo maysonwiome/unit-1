5a250b7eac4816345d7944b692aa4511a1a8bf81
maysonwiome <819689@mystma.org>
2020-04-20 15:16:54 +0000

    if statement

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,34 +2,16 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        for (int i = 22; i<=28; i = i + 3){
-            System.out.println("the count is" + i);
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i + "is divisble by 4");
+            } else {
+                System.out.println(i + "is not divisible by 4");
+
+            }
         }
 
-        for (int a = 2; a<=32; a = a * a){
-            System.out.println("the count is" + a);
-        }
-        
-        for (int b = 3; b<-81; b = b * -b){
-            System.out.println("the count is" + b);
-        }
-
-        for (int c = 50; c >= 5; c = c - 5){
-            System.out.println("the count is" + c);
-        }
-
-        System.out.println("1 4 1 4 5 2");
-        for (int d = 9; d >= 3; d = d - 3 ){
-            System.out.println("the count is" + d);
-        }
-        System.out.println("16 7 4 25 8 5 36 9 6 49 10 7 64 11 8 81 12 9");
-
-       
-
-
-
-    
-        
+        System.out.println("All finished");
     }
 }

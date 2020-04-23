c57a9c0d7fb09f1385959ee368315ca8ce5e37e4
maysonwiome <819689@mystma.org>
2020-04-23 14:56:40 +0000

    for and if test

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -3,19 +3,19 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int a = 20; a >= 0; a + -a) {
-            System.out.println(a);
-            if (a == 10) {
-                for( int j = 0; j < 5; j + -j){
-                    System.out.println(a);
-                }
+        for (int a = 20; a <= 0; a + -a){
+            System.out.println(" is double dgit");
+            if (a <= 9){
+                System.out.println(" is a single digit ");
             }
         }
-          System.out.println("first loop done");  
+          System.out.println("first loop is done");
 
-          for ( int b = 4; b <= 36; b ++) {
+          for (char b = "fast food"){
               System.out.println(b);
+              if (b = "fast food ")
           }
+          
 
       
     }

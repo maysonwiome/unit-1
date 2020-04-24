c6c18179fc79be10e6d8e1f7a10fd564ac3f6e3f
maysonwiome <819689@mystma.org>
2020-04-24 15:08:56 +0000

    quiz

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -3,20 +3,21 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int a = 20; a <= 0; a + -a){
-            System.out.println(" is double dgit");
-            if (a <= 9){
-                System.out.println(" is a single digit ");
+        for (int a = 1; a <= 10; a ++ ){
+            System.out.println(a);
+            if ( a == 5){
+                System.out.println("five");
             }
         }
-          System.out.println("first loop is done");
+        System.out.println("first loop done");
 
-          for (char b = "fast food"){
-              System.out.println(b);
-              if (b = "fast food ")
-          }
-          
-
-      
+        for (int b = 100; b <= 200; b + 10){
+            System.out.println(b);
+            if (b == 150){
+                for (b = 150; b <= 159; b ++ ){
+                    System.out.println(b);
+                }
+            }
+        }
     }
 }

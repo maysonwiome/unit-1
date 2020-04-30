a6e84df182da0f38e86f23009d3fe436ee0aa2e9
maysonwiome <819689@mystma.org>
2020-04-30 15:15:40 +0000

    scanning

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -6,13 +6,12 @@ public class App {
 
         Scanner input =  new Scanner(System.in);
 
-        System.out.println("please enter password.");
-        String correctPassword = input.nextLine();
-        System.out.println("thank you" + correctPassword);
-        System.out.println("please enter password");
-        String incorectPassword = input.nextline();
-        System.out.println("sorry" + incorectPassword);
-       
+        System.out.println("The count is");
+        String theCountIs7 = input.nextLine();
+        System.out.println("thank you" + theCountIs7);
+        System.out.println("the Count is");
+        String theCountIs1 = input.nextLine()
+        System.out.println("thank you" + theCountIs1)
 
     }
 }
